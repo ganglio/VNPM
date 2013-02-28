42d3614af89bfd9140cb15d631cea8ecba3b065e
Roberto Torella <roberto.torella@gmail.com>
2013-02-28 12:06:36 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,24 @@
+VNMP - Vagrant Nginx MySQL PHP
+================================
+*A supersimple way to have a fully fladged webserver on your machine*
+
+Prerequisites
+-------------
+
+* [VirtualBox](http://www.virtualbox.org)
+* [Vagrant](http://www.vagrantup.com). The [vagrant-vbguest plugin](http://blog.carlossanchez.eu/2012/05/03/automatically-download-and-install-virtualbox-guest-additions-in-vagrant/) is also nice to have
+
+Usage
+-----
+
+1. Clone this repo into some directory and cd into it
+2. Edit the Vagrantfile. The only thing that might need changing is the shared source folder. Have it point to some folder on your host machine that contains an index.php file.
+3. vagrant up
+4. Visit [http://localhost:8080](http://localhost:8080) to access your home folder
+5. Visit [http://localhost:8080/phpmyadmin](http://localhost:8080/phpmyadmin) for phpmyadmin
+6. SSH to localhost:2222 (or just do "vagrant ssh" if you have a decent host machine like Linux)
+
+Hints
+-----
+
+* Use "vagrant provision" to re-provision the VM anytime you make a change to the Puppet manifest or module files. Do not change anything manually in the VM!
---
+++ puppet/manifests/base.pp
@@ -0,0 +1,112 @@
+group { 'puppet':
+	ensure => present,
+}
+
+exec { 'apt-get update':
+	command => '/usr/bin/apt-get update',
+}
+
+# Let's install some packages
+
+package { 'vim':
+	ensure  => present,
+	require => Exec['apt-get update'],
+}
+
+package { 'nginx': 
+	ensure  => present,
+	require => Exec['apt-get update'],
+}
+
+package { 'mysql-server':
+	ensure  => present,
+	require => Exec['apt-get update'],
+}
+
+package { 'php5-fpm':
+	ensure  => present,
+	require => Exec['apt-get update'],
+}
+
+package { 'php5-mysql':
+	ensure  => present,
+	require => [
+		Exec['apt-get update'],
+		Package['php5-fpm'],
+		Package['mysql-server'],
+	],
+}
+
+package { 'phpmyadmin':
+	ensure  => present,
+	require => [
+		Exec['apt-get update'],
+		Package['php5-fpm'],
+		Package['php5-mysql'],
+		Package['mysql-server'],
+	],
+}
+
+# Let's start some services
+
+service { 'nginx':
+	ensure  => running,
+	require => Package['nginx'],
+}
+
+service { 'mysql':
+	ensure  => running,
+	require => Package['mysql-server'],
+}
+
+service { 'php5-fpm':
+	ensure  => running,
+	require => Package['php5-fpm'],
+}
+
+# And now we configure
+
+exec { "create-db-schema-and-user":
+	command => "/usr/bin/mysql -uroot -p -e \"create user vagrant@'localhost' identified by 'vagrant'; grant all on *.* to vagrant@'localhost'; flush privileges;\"",
+	require => Service["mysql"]
+}
+
+file { 'vagrant-nginx':
+	path    => '/etc/nginx/sites-available/vagrant',
+	ensure  => file,
+	replace => true,
+	require => Package['nginx'],
+	source  => 'puppet:///modules/nginx/vagrant',
+	notify  => Service['nginx'],
+}
+
+file { 'phpmyadmin-config':
+	path    => '/usr/share/phpmyadmin/config.inc.php',
+	ensure  => file,
+	replace => true,
+	require => [
+		Package['nginx'],
+		Package['php5-fpm'],
+		Package['phpmyadmin'],
+	],
+	source => 'puppet:///modules/phpmyadmin/config.inc.php',
+	notify => Service['php5-fpm'],
+}
+
+
+file { 'default-nginx-disable':
+	path    => '/etc/nginx/sites-enabled/default',
+	ensure  => absent,
+	require => Package['nginx'],
+}
+
+file { 'vagrant-nginx-enable':
+	path    => '/etc/nginx/sites-enabled/vagrant',
+	target  => '/etc/nginx/sites-available/vagrant',
+	ensure  => link,
+	notify  => Service['nginx'],
+	require => [
+		File['vagrant-nginx'],
+		File['default-nginx-disable'],
+	],
+}
